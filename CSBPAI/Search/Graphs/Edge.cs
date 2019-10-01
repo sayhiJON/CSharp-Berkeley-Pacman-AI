aae29454de88775322334d4e9ed093d4aa65325e
sayhiJON <jon@imawesome.org>
2019-10-01 12:49:45 -0500

    Added Node and Edge classes

---
+++ CSBPAI/Search/Graphs/Edge.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search.Graphs {
+    public class Edge : IEquatable<Edge>, IComparable<Edge> {
+        public double Weight {
+            get; private set;
+        }
+
+        public Node Node {
+            get; private set;
+        }
+
+        private int p_HashCode = (string.Empty + 1d.ToString()).GetHashCode();
+
+        public Edge() : this(null, 1d) {
+
+        }
+
+        public Edge(Node node) : this(node, 1d) {
+
+        }
+
+        public Edge(Node node, double weight) {
+            this.Node = node;
+            this.Weight = weight;
+
+            string hashCode = (node != null) ? node.Label + weight.ToString() : string.Empty + weight.ToString();
+
+            this.p_HashCode = hashCode.GetHashCode();
+        }
+
+        public int CompareTo(Edge other) {
+            if (other == null)
+                return -1;
+
+            return other.Weight.CompareTo(other.Weight);
+        }
+
+        public bool Equals(Edge other) {
+            if (other == null)
+                return false;
+
+            return this.p_HashCode == other.p_HashCode;
+        }
+
+        public override bool Equals(object obj) {
+            Edge other = obj as Edge;
+
+            if (other == null)
+                return false;
+
+            return this.p_HashCode == other.p_HashCode;
+        }
+
+        public override int GetHashCode() {
+            return this.p_HashCode;
+        }
+    }
+}
---
+++ CSBPAI/Search/Graphs/Node.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search.Graphs {
+    public class Node : IComparable<Node>, IEquatable<Node> {
+        public string Label {
+            get; private set;
+        } = string.Empty;
+
+        public List<Edge> Edges {
+            get; private set;
+        }
+
+        private int p_HashCode = string.Empty.GetHashCode();
+
+        public Node() : this(string.Empty, new List<Edge>()) {
+
+        }
+
+        public Node(string label) : this(label, new List<Edge>()) {
+
+        }
+
+        public Node(string label, List<Edge> edges) {
+            this.Label      = label;
+            this.p_HashCode = label.GetHashCode();
+            this.Edges      = new List<Edge>(edges);
+        }
+
+        public int CompareTo(Node other) {
+            if (other == null)
+                return -1;
+
+            return this.Label.CompareTo(other.Label);
+        }
+
+        public bool Equals(Node other) {
+            if (other == null)
+                return false;
+
+            return this.p_HashCode == other.p_HashCode;
+        }
+
+        public override bool Equals(object obj) {
+            Node other = obj as Node;
+
+            if (other == null)
+                return false;
+
+            return this.p_HashCode == other.p_HashCode;
+        }
+
+        public override int GetHashCode() {
+            return this.p_HashCode;
+        }
+    }
+}
